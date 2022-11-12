80b1ca01daeee918e607c1d5ccc06512e2a16e01
Joaquin Alvarez <joalvarez@infosis.com.ar>
2022-11-12 20:56:05 -0300

    chore: First spring-boot project model with maven

---
+++ d3_v1_temp/src/main/resources/archetype-resources/src/main/java/D3__AppName__Application.java
@@ -0,0 +1,12 @@
+package ${package};
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D3${AppName}Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(D3${AppName}Application.class, args);
+    }
+}
