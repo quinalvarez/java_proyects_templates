5b1a28b1f34af874f03dea2069eb544924bed9e2
Joaquin Alvarez <joalvarez@infosis.com.ar>
2022-12-10 20:49:08 -0300

    chore: change dependencies

--- d3_v1_temp/src/main/resources/archetype-resources/src/main/java/D3__AppName__Application.java
+++ d3_v1_temp/src/main/resources/archetype-resources/src/main/java/D3__AppName__Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class D3${AppName}Application {
+public class ${AppName}Application {
 
     public static void main(String[] args) {
         SpringApplication.run(D3${AppName}Application.class, args);
